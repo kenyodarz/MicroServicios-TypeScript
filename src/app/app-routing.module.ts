f95129a42c0f516b4ab01135a0b01e2f2aaea10e
Jorge Mina <minajorge@hotmail.com>
2020-06-27 14:21:52 -0500

    Create Components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
+//Angular
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+// Components
+import { CursosComponent } from './components/cursos/cursos.component';
+import { AlumnosComponent } from './components/alumnos/alumnos.component';
+import { ExamenesComponent } from './components/examenes/examenes.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'cursos', component: CursosComponent},
+  {path: 'alumnos', component: AlumnosComponent},
+  {path: 'examenes', component: ExamenesComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AlumnosComponent } from './components/alumnos/alumnos.component';
+import { CursosComponent } from './components/cursos/cursos.component';
+import { ExamenesComponent } from './components/examenes/examenes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlumnosComponent,
+    CursosComponent,
+    ExamenesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
